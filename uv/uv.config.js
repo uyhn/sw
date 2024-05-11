0125aba86b2345f30599e1324d58c08a73ca9834
uyhn <131318521+uyhn@users.noreply.github.com>
2024-05-10 19:17:07 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/assignment/',
     bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
